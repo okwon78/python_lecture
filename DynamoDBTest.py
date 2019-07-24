cad2a89161b02e5d5acfbde1d9bb98adeb0a1981
obkwon <obkwon@amorepacific.com>
2019-07-24 17:10:41 +0900

    .

--- DynamoDBTest.py
+++ DynamoDBTest.py
@@ -3,26 +3,34 @@ import boto3
 import time
 
 # day * minutes * seconds * hours
-EXPIRE_DAY = 30 * 60 * 60 * 24
+EXPIRE_DAY_OFFSET = 60 * 60 * 24  # minutes * seconds * hour
+EXPIRE_DAY = 30
+#EXPIRE = EXPIRE_DAY * EXPIRE_DAY_OFFSET
+EXPIRE = 60
+
+MAX_SEQ_LEN = 5
 
 
 class DynamoDB(object):
     def __init__(self):
         self.dynamodb = boto3.resource('dynamodb',
                                        region_name='ap-northeast-2',
-                                       aws_access_key_id='AKIAXRJ2HI73PLFWN43E',
-                                       aws_secret_access_key='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')
+                                       aws_access_key_id='',
+                                       aws_secret_access_key='')
 
-        self.table = self.dynamodb.Table('recsys-inverted-index')
+        self.table = self.dynamodb.Table('recsys-appmall-user-history')
         # print(self.table.creation_date_time)
 
     @staticmethod
     def get_ttl():
-        ttl = EXPIRE_DAY + int(time.time())
+        ttl = EXPIRE + int(time.time())
         return ttl
 
     def __put_item(self, user_id, items):
         ttl = self.get_ttl()
+
+        if len(items) > MAX_SEQ_LEN:
+            items = items[0:MAX_SEQ_LEN]
 
         self.table.put_item(
             Item={
@@ -65,13 +73,15 @@ class DynamoDB(object):
             }
         )
 
-        if 'Item' in response:
-            items = response['Item']['items']
-            return items
-
         if response['ResponseMetadata']['HTTPStatusCode'] is not 200:
             logging.error(f"Invalid HTTPStatusCode [{response['ResponseMetadata']['HTTPStatusCode']}]")
             return None
+
+        if 'Item' in response:
+            items = response['Item']['items']
+            return items
+        else
+
         else:
             return None
 
@@ -80,4 +90,15 @@ dynamodb = DynamoDB()
 
 # dynamodb.get_items("2222")
 
-dynamodb.add_item("2222", "33333")
+dynamodb.add_item("2", "1")
+dynamodb.add_item("2", "2")
+dynamodb.add_item("2", "3")
+dynamodb.add_item("2", "4")
+dynamodb.add_item("2", "5")
+dynamodb.add_item("2", "6")
+dynamodb.add_item("2", "7")
+dynamodb.add_item("2", "8")
+dynamodb.add_item("2", "9")
+dynamodb.add_item("2", "10")
+
+
